7ae4cae5df94e3005df60f03b72b478a00f531fa
wjdtndk <166589877+wjdtndk@users.noreply.github.com>
2025-02-11 20:04:13 +0900

    Add files via upload

---
+++ precache-manifest.adbcd0bc0c49600bc1bd1126b387f75a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e1eb3e2ae444aa397f36f5efcd0a88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b29e8247ae2e201db373",
+    "url": "/static/css/main.32e62b57.chunk.css"
+  },
+  {
+    "revision": "59d9bd47163b6a2576b3",
+    "url": "/static/js/2.8080122d.chunk.js"
+  },
+  {
+    "revision": "b29e8247ae2e201db373",
+    "url": "/static/js/main.511475e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
